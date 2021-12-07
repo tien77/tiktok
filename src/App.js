ed9322b13a47223caec542ba0245be95484dbe23
tien77 <tranminhtien22@gmail.com>
2021-12-07 22:39:07 +0700

    todolist

--- src/App.js
+++ src/App.js
@@ -90,43 +90,80 @@ function App() {
     </div>
   ); */
 
+  /*
+// submit form type check
+const [checked, setChecked] = useState([])
+console.log(checked)
+const handleCheck = (id) => {
+  setChecked(prev => {
+    const isChecked = checked.includes(id)
+    if (isChecked) {
+      return checked.filter( item => item !== id )
+      // method filter() trả về arr mới thỏa điều kiện
+    } else {
+      return [...prev, id]
+      //toán tử prev là giá trị trước đó, ...prev là giải mảng trước đó và truyền id mới nữa vào mảng
+    }
+  })
+}
+const handleSubmit = () => {
+  //handle call API
+  console.log('Ids: ', checked);
+}
+return (
+  <div className="App" style={{ padding: 20 }}>
+    {
+      courses.map(course => (
+        <div key={course.id} >
+          <input
+            type="checkbox"
+            checked={checked.includes(course.id)}
+              //includes method trả về true nếu mảng checked có giá trị id truyền vào, ngược lại là false
+            onChange={() => handleCheck(course.id)}
+          />
+          <label>{course.name}</label>
+        </div>
+      ))
+    }
+    <button onClick={handleSubmit}> Submit </button>
+  </div>
+); */
 
-  // submit form type check
-  const [checked, setChecked] = useState([])
-    
-  console.log(checked)
-  const handleCheck = (id) => {
-    setChecked(prev => {
-      const isChecked = checked.includes(id)
-      if (isChecked) {
-        return checked.filter( item => item !== id )
-        // method filter() trả về arr mới thỏa điều kiện
-      } else {
-        return [...prev, id]
-        //toán tử prev là giá trị trước đó, ...prev là giải mảng trước đó và truyền id mới nữa vào mảng
-      }
-    })
-  }
+  //example: todolist
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState( () => {
+
+    // get value in Local Storage
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    return storageJobs  ?? []
+    // toán tử ?? (nullish), nếu storageJobs là null hoặc undefined thì sẽ lấy vế sau là []
+  })
+
   const handleSubmit = () => {
-    //handle call API
-    console.log('Ids: ', checked);
+    setJobs( prev => {
+      const newJobs = [...prev, job]
+
+      // save to local storage
+      const jsonJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs',jsonJobs )
+
+      return newJobs
+    } )
+    setJob('')
   }
+
   return (
     <div className="App" style={{ padding: 20 }}>
-      {
-        courses.map(course => (
-          <div key={course.id} >
-            <input
-              type="checkbox"
-              checked={checked.includes(course.id)}
-                //includes method trả về true nếu mảng checked có giá trị id truyền vào, ngược lại là false
-              onChange={() => handleCheck(course.id)}
-            />
-            <label>{course.name}</label>
-          </div>
-        ))
-      }
-      <button onClick={handleSubmit}> Submit </button>
+      <input 
+        value={job}
+        onChange={e=>setJob(e.target.value)}
+      />
+      <button onClick={handleSubmit} > Add </button>
+      <ul>
+        {jobs.map( (job, index) =>
+          <li key={index}>{job}</li>
+        )}
+      </ul>
     </div>
   );
 
